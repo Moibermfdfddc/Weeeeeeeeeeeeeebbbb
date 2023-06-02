53e86d27d87678ed491f188439cc9434fb7220d9
Bassel196 <125680928+Bassel196@users.noreply.github.com>
2023-06-02 18:42:27 +0300

    redirect

--- app.js
+++
@@ -1,7 +0,0 @@
-require("dotenv");
-const express = require("express");
-const app = express()
-const port=8005;
-app.listen(port,()=>{
-    console.log(`server is running on port number ${port}`)
-})
